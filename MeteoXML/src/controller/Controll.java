cf22890913b82ab5f2ce322aa017bb79052a2156
Nicola <Nicola@PCDELLNIKO>
2018-01-07 18:36:23 +0100

    Risolto problema mancanza connessione!

--- MeteoXML/src/controller/Controll.java
+++ MeteoXML/src/controller/Controll.java
@@ -4,7 +4,10 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 
 import javax.sound.sampled.AudioInputStream;
@@ -56,168 +59,178 @@ public class Controll implements ActionListener{
 	@Override
 	public void actionPerformed(ActionEvent ev) {
 		if(w.getBtnNow()==ev.getSource()) {
-			try {
-				String s="";
-				String city=""+w.getTfCitta().getText();
-				String url="http://api.openweathermap.org/data/2.5/weather?q="+city+"&mode=xml&appid="+apiKey;
-				URL xmlFile = new URL(url);
-				// Inizializzo Jaxb per dire che dovrà leggere l'xml in un certo modo
-				JAXBContext jaxbContext = JAXBContext.newInstance(Current.class);
-				// Il unmarshaller è l'oggetto che legge l'xml
-				Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
-				Current c = (Current) jaxbUnmarshaller.unmarshal(xmlFile);
-				w.getLblLatitudine2().setText(""+c.getCity().getCoord().getLat());
-				w.getLblLongitudine2().setText(""+c.getCity().getCoord().getLon());
-				w.getLblData2().setText(c.getLastupdate().getValue().getDay()+"/"+
-										c.getLastupdate().getValue().getMonth()+"/"+
-										c.getLastupdate().getValue().getYear());
-				s=""+c.getLastupdate().getValue();
-				for(int i=0;i<=10;i++) {
-					s=removeCharAt(s,0);
-				}
-				w.getLblOra2().setText(s);
-				w.getLblID2().setText(""+c.getCity().getId());
-				
-				//String unit=" "+c.getTemperature().getUnit();
-				String unit="Celsius";
-				s=""+Float.parseFloat(""+(Float.parseFloat(""+c.getTemperature().getValue())-273));
-				s=arrotondamento(s);
-				w.getLblTemperatura2().setText(""+s+" "+unit);
-				
-				s=""+Float.parseFloat(""+(Float.parseFloat(""+c.getTemperature().getMax())-273));
-				s=arrotondamento(s);
-				w.getLblTemperaturaMassima2().setText(""+s+" "+unit);
-				
-				s=""+Float.parseFloat(""+(Float.parseFloat(""+c.getTemperature().getMin())-273));
-				s=arrotondamento(s);
-				w.getLblTemperaturaMinima2().setText(""+s+" "+unit);
-				
-				unit=" "+c.getHumidity().getUnit();
-				w.getLblUmidita2().setText(""+c.getHumidity().getValue()+unit);
-				unit=" "+c.getPressure().getUnit();
-				w.getLblPressione2().setText(""+c.getPressure().getValue()+unit);
-				
-				w.getLblNomeVento2().setText(""+c.getWind().getSpeed().getName());
-				w.getLblVelocitaVento2().setText(""+c.getWind().getSpeed().getValue());
-				w.getLblDirezioneVento2().setText(""+c.getWind().getDirection().getValue()+
-						" "+c.getWind().getDirection().getName());
-				
-				System.out.println(c.getCity().getName());
-
-			} catch (Exception e) {
-				JOptionPane.showMessageDialog(w, "Città NON Trovata.", "NotFound", 0);
-			}
-		}
-		else if(w.getBtn5Day()==ev.getSource()) {
-			try {
-				String s="";
-				String city=""+w.getTfCitta().getText();
-				String url="http://api.openweathermap.org/data/2.5/forecast?q="+city+"&mode=xml&appid="+apiKey;
-				//String url="Forecast.xml";
-				URL xmlFile = new URL(url);
-				// Inizializzo Jaxb per dire che dovrà leggere l'xml in un certo modo
-				JAXBContext jaxbContext = JAXBContext.newInstance(Weatherdata.class);
-				// Il unmarshaller è l'oggetto che legge l'xml
-				Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
-				Weatherdata c = (Weatherdata) jaxbUnmarshaller.unmarshal(xmlFile);
-				w.getLblLatitudine2().setText(""+c.getLocation().getLocation().getLatitude());
-				w.getLblLongitudine2().setText(""+c.getLocation().getLocation().getLongitude());
-				
-				ArrayList<String> momentFrom=new ArrayList<String>();
-				ArrayList<String> momentTo=new ArrayList<String>();
-				for(int i=0;i<c.getForecast().getTime().size();i++) {
-					s=""+c.getForecast().getTime().get(i).getFrom();
-					for(int j=0;j<=10;j++) {
-						s=removeCharAt(s,0);
-					}
-					momentFrom.add(""+c.getForecast().getTime().get(i).getFrom().getDay()+"/"+
-							c.getForecast().getTime().get(i).getFrom().getMonth()+"/"+
-							c.getForecast().getTime().get(i).getFrom().getYear()+"      "+s);
-					
-					s=""+c.getForecast().getTime().get(i).getTo();
-					for(int j=0;j<=10;j++) {
-						s=removeCharAt(s,0);
-					}
-					momentTo.add(""+c.getForecast().getTime().get(i).getTo().getDay()+"/"+
-							c.getForecast().getTime().get(i).getTo().getMonth()+"/"+
-							c.getForecast().getTime().get(i).getTo().getYear()+"      "+s);
-				}
-				JScrollPane scrollPane=new JScrollPane();
-				JTable table=new JTable();
-				scrollPane.setViewportView(table);
-				DefaultTableModel model=new DefaultTableModel(){@Override
-					public boolean isCellEditable(int row, int col) {
-					return false;
-					}
-				};
-				Object[] colums={"Da","A"};
-				table.setModel(model);
-				model.setColumnIdentifiers(colums);
-				table.getColumnModel().getColumn(0).setMinWidth(150);
-				table.getColumnModel().getColumn(0).setMaxWidth(150);
-				table.getColumnModel().getColumn(0).setPreferredWidth(150);
-				table.getColumnModel().getColumn(1).setMinWidth(150);
-				table.getColumnModel().getColumn(1).setMaxWidth(150);
-				table.getColumnModel().getColumn(1).setPreferredWidth(150);
-				Object[] rows=new Object[2];
-				for(int i=0;i<c.getForecast().getTime().size();i++) {
-					rows[0]=momentFrom.get(i);
-					rows[1]=momentTo.get(i);
-					model.addRow(rows);
-					table.setRowHeight(30);
-				}
-				scrollPane.setPreferredSize(new Dimension(308, 200));
-				JPanel pnl=new JPanel();
-				pnl.setBackground(Color.CYAN);
-				scrollPane.setOpaque(false);
-				pnl.add(scrollPane);
-				JOptionPane.showMessageDialog(w, pnl, "Seleziona Periodo", 1);
+			if(connection()) {
 				try {
-					int sel=table.getSelectedRow();
-					
-					w.getLblID2().setText(""+c.getLocation().getLocation().getGeobaseid());
-					
-					w.getLblData2().setText(""+c.getForecast().getTime().get(sel).getFrom().getDay()+"/"+
-											c.getForecast().getTime().get(sel).getFrom().getMonth()+"/"+
-											c.getForecast().getTime().get(sel).getFrom().getYear());
-					s=""+c.getForecast().getTime().get(sel).getFrom();
+					String s="";
+					String city=""+w.getTfCitta().getText();
+					String url="http://api.openweathermap.org/data/2.5/weather?q="+city+"&mode=xml&appid="+apiKey;
+					URL xmlFile = new URL(url);
+					// Inizializzo Jaxb per dire che dovrà leggere l'xml in un certo modo
+					JAXBContext jaxbContext = JAXBContext.newInstance(Current.class);
+					// Il unmarshaller è l'oggetto che legge l'xml
+					Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+					Current c = (Current) jaxbUnmarshaller.unmarshal(xmlFile);
+					w.getLblLatitudine2().setText(""+c.getCity().getCoord().getLat());
+					w.getLblLongitudine2().setText(""+c.getCity().getCoord().getLon());
+					w.getLblData2().setText(c.getLastupdate().getValue().getDay()+"/"+
+											c.getLastupdate().getValue().getMonth()+"/"+
+											c.getLastupdate().getValue().getYear());
+					s=""+c.getLastupdate().getValue();
 					for(int i=0;i<=10;i++) {
 						s=removeCharAt(s,0);
 					}
 					w.getLblOra2().setText(s);
-
-					s="";
+					w.getLblID2().setText(""+c.getCity().getId());
+					
+					//String unit=" "+c.getTemperature().getUnit();
 					String unit="Celsius";
-					s=""+(Float.parseFloat(""+c.getForecast().getTime().get(sel).getTemperature().getValue())-273);
+					s=""+Float.parseFloat(""+(Float.parseFloat(""+c.getTemperature().getValue())-273));
 					s=arrotondamento(s);
 					w.getLblTemperatura2().setText(""+s+" "+unit);
 					
-					s=""+(Float.parseFloat(""+c.getForecast().getTime().get(sel).getTemperature().getMax())-273);
+					s=""+Float.parseFloat(""+(Float.parseFloat(""+c.getTemperature().getMax())-273));
 					s=arrotondamento(s);
 					w.getLblTemperaturaMassima2().setText(""+s+" "+unit);
 					
-					s=""+(Float.parseFloat(""+c.getForecast().getTime().get(sel).getTemperature().getMin())-273);
+					s=""+Float.parseFloat(""+(Float.parseFloat(""+c.getTemperature().getMin())-273));
 					s=arrotondamento(s);
 					w.getLblTemperaturaMinima2().setText(""+s+" "+unit);
 					
-					unit=" "+c.getForecast().getTime().get(sel).getHumidity().getUnit();
-					w.getLblUmidita2().setText(""+c.getForecast().getTime().get(sel).getHumidity().getValue()+unit);
-					unit=" "+c.getForecast().getTime().get(sel).getPressure().getUnit();
-					w.getLblPressione2().setText(""+c.getForecast().getTime().get(sel).getPressure().getValue()+unit);
+					unit=" "+c.getHumidity().getUnit();
+					w.getLblUmidita2().setText(""+c.getHumidity().getValue()+unit);
+					unit=" "+c.getPressure().getUnit();
+					w.getLblPressione2().setText(""+c.getPressure().getValue()+unit);
 					
-					w.getLblNomeVento2().setText(""+c.getForecast().getTime().get(sel).getWindSpeed().getName());
-					w.getLblVelocitaVento2().setText(""+c.getForecast().getTime().get(sel).getWindSpeed().getMps());
-					w.getLblDirezioneVento2().setText(""+c.getForecast().getTime().get(sel).getWindDirection().getDeg()+
-							" "+c.getForecast().getTime().get(sel).getWindDirection().getName());
+					w.getLblNomeVento2().setText(""+c.getWind().getSpeed().getName());
+					w.getLblVelocitaVento2().setText(""+c.getWind().getSpeed().getValue());
+					w.getLblDirezioneVento2().setText(""+c.getWind().getDirection().getValue()+
+							" "+c.getWind().getDirection().getName());
+					
+					System.out.println(c.getCity().getName());
+	
+				} catch (Exception e) {
+					JOptionPane.showMessageDialog(w, "Città NON Trovata.", "NotFound", 0);
 				}
-				catch(Exception e) {
-					System.out.println("Error Selection");
+			}
+			else {
+				JOptionPane.showMessageDialog(w, "Non c'è connessione ad Internet!", "ERR_INTERNET_DISCONNECTED", 0);
+			}
+		}
+		else if(w.getBtn5Day()==ev.getSource()) {
+			if(connection()) {
+				try {
+					String s="";
+					String city=""+w.getTfCitta().getText();
+					String url="http://api.openweathermap.org/data/2.5/forecast?q="+city+"&mode=xml&appid="+apiKey;
+					//String url="Forecast.xml";
+					URL xmlFile = new URL(url);
+					// Inizializzo Jaxb per dire che dovrà leggere l'xml in un certo modo
+					JAXBContext jaxbContext = JAXBContext.newInstance(Weatherdata.class);
+					// Il unmarshaller è l'oggetto che legge l'xml
+					Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+					Weatherdata c = (Weatherdata) jaxbUnmarshaller.unmarshal(xmlFile);
+					w.getLblLatitudine2().setText(""+c.getLocation().getLocation().getLatitude());
+					w.getLblLongitudine2().setText(""+c.getLocation().getLocation().getLongitude());
+					
+					ArrayList<String> momentFrom=new ArrayList<String>();
+					ArrayList<String> momentTo=new ArrayList<String>();
+					for(int i=0;i<c.getForecast().getTime().size();i++) {
+						s=""+c.getForecast().getTime().get(i).getFrom();
+						for(int j=0;j<=10;j++) {
+							s=removeCharAt(s,0);
+						}
+						momentFrom.add(""+c.getForecast().getTime().get(i).getFrom().getDay()+"/"+
+								c.getForecast().getTime().get(i).getFrom().getMonth()+"/"+
+								c.getForecast().getTime().get(i).getFrom().getYear()+"      "+s);
+						
+						s=""+c.getForecast().getTime().get(i).getTo();
+						for(int j=0;j<=10;j++) {
+							s=removeCharAt(s,0);
+						}
+						momentTo.add(""+c.getForecast().getTime().get(i).getTo().getDay()+"/"+
+								c.getForecast().getTime().get(i).getTo().getMonth()+"/"+
+								c.getForecast().getTime().get(i).getTo().getYear()+"      "+s);
+					}
+					JScrollPane scrollPane=new JScrollPane();
+					JTable table=new JTable();
+					scrollPane.setViewportView(table);
+					DefaultTableModel model=new DefaultTableModel(){@Override
+						public boolean isCellEditable(int row, int col) {
+						return false;
+						}
+					};
+					Object[] colums={"Da","A"};
+					table.setModel(model);
+					model.setColumnIdentifiers(colums);
+					table.getColumnModel().getColumn(0).setMinWidth(150);
+					table.getColumnModel().getColumn(0).setMaxWidth(150);
+					table.getColumnModel().getColumn(0).setPreferredWidth(150);
+					table.getColumnModel().getColumn(1).setMinWidth(150);
+					table.getColumnModel().getColumn(1).setMaxWidth(150);
+					table.getColumnModel().getColumn(1).setPreferredWidth(150);
+					Object[] rows=new Object[2];
+					for(int i=0;i<c.getForecast().getTime().size();i++) {
+						rows[0]=momentFrom.get(i);
+						rows[1]=momentTo.get(i);
+						model.addRow(rows);
+						table.setRowHeight(30);
+					}
+					scrollPane.setPreferredSize(new Dimension(308, 200));
+					JPanel pnl=new JPanel();
+					pnl.setBackground(Color.CYAN);
+					scrollPane.setOpaque(false);
+					pnl.add(scrollPane);
+					JOptionPane.showMessageDialog(w, pnl, "Seleziona Periodo", 1);
+					try {
+						int sel=table.getSelectedRow();
+						
+						w.getLblID2().setText(""+c.getLocation().getLocation().getGeobaseid());
+						
+						w.getLblData2().setText(""+c.getForecast().getTime().get(sel).getFrom().getDay()+"/"+
+												c.getForecast().getTime().get(sel).getFrom().getMonth()+"/"+
+												c.getForecast().getTime().get(sel).getFrom().getYear());
+						s=""+c.getForecast().getTime().get(sel).getFrom();
+						for(int i=0;i<=10;i++) {
+							s=removeCharAt(s,0);
+						}
+						w.getLblOra2().setText(s);
+	
+						s="";
+						String unit="Celsius";
+						s=""+(Float.parseFloat(""+c.getForecast().getTime().get(sel).getTemperature().getValue())-273);
+						s=arrotondamento(s);
+						w.getLblTemperatura2().setText(""+s+" "+unit);
+						
+						s=""+(Float.parseFloat(""+c.getForecast().getTime().get(sel).getTemperature().getMax())-273);
+						s=arrotondamento(s);
+						w.getLblTemperaturaMassima2().setText(""+s+" "+unit);
+						
+						s=""+(Float.parseFloat(""+c.getForecast().getTime().get(sel).getTemperature().getMin())-273);
+						s=arrotondamento(s);
+						w.getLblTemperaturaMinima2().setText(""+s+" "+unit);
+						
+						unit=" "+c.getForecast().getTime().get(sel).getHumidity().getUnit();
+						w.getLblUmidita2().setText(""+c.getForecast().getTime().get(sel).getHumidity().getValue()+unit);
+						unit=" "+c.getForecast().getTime().get(sel).getPressure().getUnit();
+						w.getLblPressione2().setText(""+c.getForecast().getTime().get(sel).getPressure().getValue()+unit);
+						
+						w.getLblNomeVento2().setText(""+c.getForecast().getTime().get(sel).getWindSpeed().getName());
+						w.getLblVelocitaVento2().setText(""+c.getForecast().getTime().get(sel).getWindSpeed().getMps());
+						w.getLblDirezioneVento2().setText(""+c.getForecast().getTime().get(sel).getWindDirection().getDeg()+
+								" "+c.getForecast().getTime().get(sel).getWindDirection().getName());
+					}
+					catch(Exception e) {
+						System.out.println("Error Selection");
+					}
+					
+					System.out.println(c.getLocation().getName());
+	
+				} catch (Exception e) {
+					JOptionPane.showMessageDialog(w, "Città NON Trovata.", "NotFound", 0);
 				}
-				
-				System.out.println(c.getLocation().getName());
-
-			} catch (Exception e) {
-				JOptionPane.showMessageDialog(w, "Città NON Trovata.", "NotFound", 0);
+			}
+			else {
+				JOptionPane.showMessageDialog(w, "Non c'è connessione ad Internet!", "ERR_INTERNET_DISCONNECTED", 0);
 			}
 		}
 		else if(w.getBtnClear()==ev.getSource()) {
@@ -265,5 +278,18 @@ public class Controll implements ActionListener{
 		}
 		
 		return s2;
+	}
+	
+	public static boolean connection() {
+		try {
+			URL url = new URL("http://www.google.com");
+			URLConnection uc = url.openConnection();
+			BufferedReader br = new BufferedReader(new InputStreamReader(uc.getInputStream()));
+			String line;
+			line = br.readLine();
+			if(line!=null)
+				return true;
+		} catch(Exception e) {}
+		return false;
 	}
 }
